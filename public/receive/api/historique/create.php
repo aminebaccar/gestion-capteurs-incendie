ea19b3dec1dcaa0acba52bd64dd669e0fe2b1639
aminebaccar <aminebaccar@users.noreply.github.com>
2019-03-24 16:05:59 +0100

    added alert interval

--- public/receive/api/historique/create.php
+++ public/receive/api/historique/create.php
@@ -20,6 +20,19 @@
   $historique->created_at = $data->created_at;
   $historique->updated_at = $data->updated_at;
 
+  $query0 = 'SELECT created_at
+    FROM historiques
+    order by created_at desc
+    limit 1';
+  $stmt0 = $historique->getConn()->prepare($query0);
+  $stmt0->execute();
+  $row0 = $stmt0->fetch(PDO::FETCH_ASSOC);
+  $last = $row0['created_at'];
+  $lasttime = strtotime($last);
+
+  $minutes = abs(strtotime($historique->created_at) - $lasttime) / 60;
+  if($minutes > 60)
+{
   if($historique->create()) {
     echo json_encode(
       array('message' => 'historique ajouté')
@@ -29,15 +42,16 @@
 		'clientSecret' => 'ITjYt1ENgZUxHc99'
 	  ];
 	  $osms = new Osms\Osms($credential);
-
-
     $token = $osms->getTokenFromConsumerKey();
+
+
     $query = 'SELECT distinct telephone
       FROM users inner join capteurs on (users.etab=capteurs.etab) inner join historiques on (capteurs.code_capteur=historiques.capteur)
       where historiques.capteur='.$historique->capteur;
     $stmt = $historique->getConn()->prepare($query);
     $stmt->execute();
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+    {
       $osms->sendSMS('tel:+21693233173', 'tel:+216' . $row['telephone'], "Capteur ".$historique->capteur. " est en incendie", 'Baccar');
     }
 
@@ -49,3 +63,4 @@
         array('message' => 'historique pas ajouté')
       );
     }
+}
