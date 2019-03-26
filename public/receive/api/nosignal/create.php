80148f500c4411478e3b2194fe8034f47884632a
aminebaccar <aminebaccar@users.noreply.github.com>
2019-03-26 15:15:43 +0100

    testing

--- public/receive/api/nosignal/create.php
+++ public/receive/api/nosignal/create.php
@@ -42,7 +42,7 @@ echo $row1['capteur'];
     $lasttime = strtotime($last);
 
     $minutes = abs(strtotime($historique->created_at) - $lasttime) / 60;
-    echo "    ".$minutes;
+    echo " BONJOUR   ".$minutes;
     if($minutes > 60)
     {
     if($historique->create()) {
