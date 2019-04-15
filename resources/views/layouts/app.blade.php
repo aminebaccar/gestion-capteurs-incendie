f21cc87b5b0ae39c3f6076faf238ec2aab91d963
aminebaccar <aminebaccar@users.noreply.github.com>
2019-04-15 08:39:55 +0100

    edited styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
       width:100%;
       padding:10px;
     }
-
+    </style>
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.js"></script>
