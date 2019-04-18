a93f96354d0a844844342eeb99786899f181d315
aminebaccar <aminebaccar@users.noreply.github.com>
2019-04-18 21:29:04 +0100

    fixing bugs

--- public/receive/api/historique/create.php
+++ public/receive/api/historique/create.php
@@ -67,7 +67,7 @@
 		$e = $st->fetch(PDO::FETCH_ASSOC);
 
 echo "BONSOIIRRRRRRR".$e['etab'];
-    $ourouru = 'SELECT id from etablissements where nom like " '.$e['etab'].' " limit 1';
+    $ourouru = 'SELECT id from etablissements where nom like "'.$e['etab'].'" limit 1';
     $oro = $historique->getConn()->prepare($ourouru);
     $oro->execute();
     $ror=$oro->fetch(PDO::FETCH_ASSOC);
