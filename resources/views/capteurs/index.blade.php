9f8923cd19f18e4f07cd48a23f2c1d6b13810255
aminebaccar <aminebaccar@users.noreply.github.com>
2019-04-29 18:45:45 +0200

    fixed bugs

--- resources/views/capteurs/index.blade.php
+++ resources/views/capteurs/index.blade.php
@@ -129,7 +129,7 @@ $(document).ready(function() {
                             <form action="{{ route('capteurs.destroy', $capteur->id) }}" method="POST">
 {{ method_field('DELETE') }}
 {{ csrf_field() }}
-<button type='submit' class="btn btn-danger" style="	background: none; onclick="return confirm('Are you sure?')"
+<button type='submit' class="btn btn-danger" onclick="return confirm('Are you sure?')" style="	background: none; 
 	color: #9aa0ac;
 	border: none;
 	padding: 0;
