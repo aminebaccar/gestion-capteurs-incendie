4c2420bcd0d0778e40268a973a9fe8d3c750f78d
aminebaccar <aminebaccar@users.noreply.github.com>
2019-05-01 01:42:11 +0200

    testing

--- resources/views/etablissements/index.blade.php
+++ resources/views/etablissements/index.blade.php
@@ -86,51 +86,9 @@
     for (var i=0;i<aTags.length;i++){
         aTags[i].addEventListener('click', function(e){
           e.preventDefault();
-          bootbox.setDefaults({
-  /**
-   * @optional String
-   * @default: en
-   * which locale settings to use to translate the three
-   * standard button labels: OK, CONFIRM, CANCEL
-   */
-  locale: "fr",
-
-  /**
-   * @optional Boolean
-   * @default: true
-   * whether the dialog should be shown immediately
-   */
-  show: true,
-
-  /**
-   * @optional Boolean
-   * @default: true
-   * whether the dialog should be have a backdrop or not
-   */
-  backdrop: true,
-
-  /**
-   * @optional Boolean
-   * @default: true
-   * show a close button
-   */
-  closeButton: false,
-
-  /**
-   * @optional Boolean
-   * @default: true
-   * animate the dialog in and out (not supported in < IE 10)
-   */
-  animate: true,
-
-  /**
-   * @optional String
-   * @default: null
-   * an additional class to apply to the dialog wrapper
-   */
-  className: "my-modal"
-
-});
+          bootbox.dialog({
+            closeButton: false 
+        });
           bootbox.confirm({
     message: "This is a confirm with custom button text and color! Do you like it?",
     buttons: {
