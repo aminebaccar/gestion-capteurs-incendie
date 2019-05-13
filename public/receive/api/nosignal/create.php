ac608c9e67a45eb91489084106770d21325af21c
aminebaccar <aminebaccar@users.noreply.github.com>
2019-05-13 05:23:49 +0200

    testing

--- public/receive/api/nosignal/create.php
+++ public/receive/api/nosignal/create.php
@@ -68,7 +68,58 @@ echo $row1['capteur'];
       {
         $osms->sendSMS('tel:+21693233173', 'tel:+216' . $row['telephone'], "Capteur ".$historique->capteur. " n'a pas de signal", 'Baccar');
       }
+///////////////////////////////////PUSH NOTIFICATION/////////////////////////
 
+    $content = array(
+        "en" => 'Capteur '.$historique->capteur. ' est en incendie'
+        );
+
+		$q = 'SELECT etab from capteurs where code_capteur='.$historique->capteur.' limit 1';
+		$st = $historique->getConn()->prepare($q);
+		$st->execute();
+		$e = $st->fetch(PDO::FETCH_ASSOC);
+
+
+    $ourouru = 'SELECT id from etablissements where nom like "'.$e['etab'].'" limit 1';
+    $oro = $historique->getConn()->prepare($ourouru);
+    $oro->execute();
+    $ror=$oro->fetch(PDO::FETCH_ASSOC);
+
+
+
+    $fields = array(
+        'app_id' => "a9e53700-31c3-4189-b2fc-5fbbc4634949",
+	      'filters' => array(array("field" => "tag", "key" => "etab", "relation" => "=", "value" => $ror['id'])),
+        'data' => array("foo" => "bar"),
+        'large_icon' =>"ic_launcher_round.png",
+        'contents' => $content
+    );
+
+    $fields = json_encode($fields);
+    print("\nJSON sent:\n");
+    print($fields);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+                                               'Authorization: Basic OGMwNjg1OWMtNzhjMi00NjYxLWJhZDEtODliNjM1YmYwODY2'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+$return["allresponses"] = $response;
+$return = json_encode( $return);
+print("\n\nJSON received:\n");
+print($return);
+print("\n");
+
+
+///////////////////////////////////PUSH NOTIFICATION/////////////////////////
       }
       else {
         echo json_encode(
