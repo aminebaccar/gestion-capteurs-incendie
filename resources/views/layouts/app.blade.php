a02c5e7db510e6c2c80b28045df946c50558b133
aminebaccar <aminebaccar@users.noreply.github.com>
2019-06-02 18:55:42 +0200

    testing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -175,10 +175,10 @@
                                 </li>
                                 @if(Auth::user()->usertype=="super" || Auth::user()->usertype=="admin")
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fe fe-file"></i> Interventions</a>
+                                    <a class="nav-link {{ request()->is('interventions') || request()->is('type_intervs')  ? 'active' : ''}} dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fe fe-file"></i> Interventions</a>
                                     <div class="dropdown-menu dropdown-menu-arrow">
-                                    <a href="/interventions" class="nav-link ">Interventions</a>
-                                    <a href="/type_intervs" class="nav-link ">Types Intervention</a>
+                                    <a href="/interventions" class="dropdown-item ">Interventions</a>
+                                    <a href="/type_intervs" class="dropdown-item ">Types Intervention</a>
                                     </div>
                                 </li>
                                 @else
