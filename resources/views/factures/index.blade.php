616ec20785124eaf400d75c84639fc92bb0ab077
aminebaccar <aminebaccar@users.noreply.github.com>
2019-06-10 13:56:59 +0200

    testing

--- resources/views/factures/index.blade.php
+++ resources/views/factures/index.blade.php
@@ -59,7 +59,7 @@
 
                             <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <!-- Identify your business so that you can collect the payments. -->
-        <input type="hidden" name="business" value="Yourbusinessemail@paypal.com"> <!-- Add your PayPal Seller/Business email address Required-->
+        <input type="hidden" name="business" value="besrour_2010@live.com"> <!-- Add your PayPal Seller/Business email address Required-->
         <!-- Specify a Buy Now button. -->
         <input type="hidden" name="cmd" value="_xclick">
         <!-- Specify details about the item that buyers will purchase. -->
