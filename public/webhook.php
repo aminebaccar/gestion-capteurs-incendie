04799aea00519bd45dafa521e5ff097f86a5ad50
aminebaccar <aminebaccar@users.noreply.github.com>
2019-06-11 14:36:41 +0200

    testing

---
+++ public/webhook.php
@@ -0,0 +1,28 @@
+<?php
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+  
+	$servername = "api.tangorythm.com";
+	$username = "sdiuser";
+	$password = "Sdi2019user";
+	$dbname = "sdi";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+  $data = json_decode(file_get_contents("php://input"));
+  $e = $data->event_type;
+  $to      = 'mohamedaminebaccar@gmail.com';
+$subject = 'Paypal';
+$message = $e;
+
+
+mail($to, $subject, $message, $headers);
+
+  
+  
+?>
