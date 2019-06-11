7bf1c1795477a428e31ccb56953cededeb937640
aminebaccar <aminebaccar@users.noreply.github.com>
2019-06-11 09:37:04 +0200

    testing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -203,7 +203,7 @@
             </div>
         @endauth
 
-        <main class="py-4" style="min-height:70vh;">
+        <main class="py-4" style="min-height:75vh;">
           <div class="container">
             @yield('content')
           </div>
