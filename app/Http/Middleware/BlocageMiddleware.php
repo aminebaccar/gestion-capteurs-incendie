3dd79d514050b1b339d3d1e8c48edbbedda12d26
aminebaccar <aminebaccar@users.noreply.github.com>
2019-06-11 09:03:08 +0200

    testing

--- app/Http/Middleware/BlocageMiddleware.php
+++ app/Http/Middleware/BlocageMiddleware.php
@@ -13,11 +13,12 @@ class BlocageMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next){
     if (Auth::check() && Auth::user()->bloque==0) {
         return $next($request);
     }
     else {
       return redirect('/blocage');
     }
+}
 }
